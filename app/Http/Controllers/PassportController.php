d0da431b2d683210b46dadebdefab81243f1fe44
OlegOKovqlyov <koa2003@ukr.net>
2018-08-30 10:27:57 +0300

    In the previous commit the page http://127.0.0.1:8000/passports/create was created. In the commit the page http://127.0.0.1:8000/passports was created

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -13,7 +13,8 @@ class PassportController extends Controller
      */
     public function index()
     {
-        //
+        $passports=\App\Passport::all();
+        return view('index',compact('passports'));
     }
 
     /**
@@ -34,7 +35,24 @@ class PassportController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if($request->hasfile('filename'))
+         {
+            $file = $request->file('filename');
+            $name=time().$file->getClientOriginalName();
+            $file->move(public_path().'/images/', $name);
+         }
+        $passport= new \App\Passport;
+        $passport->name=$request->get('name');
+        $passport->email=$request->get('email');
+        $passport->number=$request->get('number');
+        $date=date_create($request->get('date'));
+        $format = date_format($date,"Y-m-d");
+        $passport->date = strtotime($format);
+        $passport->office=$request->get('office');
+        $passport->filename=$name;
+        $passport->save();
+        
+        return redirect('passports')->with('success', 'Information has been added');
     }
 
     /**
